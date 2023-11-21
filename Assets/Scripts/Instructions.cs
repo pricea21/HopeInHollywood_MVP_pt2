72d9b0a57a70e92a9f1bbc83ca491538f52636c0
pricea21 <pricea26@my.erau.edu>
2023-11-20 23:16:30 -0700

    assets

---
+++ Assets/Scripts/Instructions.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Instructions : MonoBehaviour
+{
+    public GameObject instructionsUI;
+    public GameObject[] GameUI;
+    public GameObject pauseMenu;
+    public GameObject volumeControls;
+    public GameObject startButton;
+    public GameObject settingsButton;
+    public static bool GameIsPaused = false;
+    
+
+    void Update()
+    {
+        
+    }
+
+
+    public void Exit()
+    {
+        Time.timeScale = 1f;
+        instructionsUI.SetActive(false);
+        volumeControls.SetActive(false);
+        startButton.SetActive(true);
+        settingsButton.SetActive(true);
+    }
+
+    public void VolumeControl()
+    {
+        instructionsUI.SetActive(false);
+        startButton.SetActive(false);
+        pauseMenu.SetActive(false);
+        settingsButton.SetActive(true);
+        volumeControls.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void BackToInstructions()
+    {
+        volumeControls.SetActive(false);
+        instructionsUI.SetActive(true);
+    }
+
+    public void SettingsButton()
+    {
+        instructionsUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+}
+
